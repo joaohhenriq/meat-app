af21fded8f5a1e84995f757a46a7b69bcc12175c
joaohhenriq <joao_camargo10@hotmail.com>
2018-09-25 23:53:18 -0300

    Adição do Post para incluir a compra na base de dados

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -3,6 +3,7 @@ import { OrderService } from './order.service';
 import { OrderItemsComponent } from './order-items/order-items.component';
 import { RadioOption } from './../shared/radio/radio-option.model';
 import { Component, OnInit } from '@angular/core';
+import { Order, OrderItem } from './order.model';
 
 @Component({
   selector: 'mt-order',
@@ -42,6 +43,13 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem){
     this.orderService.remove(item)
+  }
+
+  checkOrder(order: Order){
+    order.orderItems = this.cartItems()
+      .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+    this.orderService.checkOrder(order)
+    console.log(order)
   }
 
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,14 @@
+import { Http } from '@angular/http';
 import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
 import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
 import { Injectable } from "../../../node_modules/@angular/core";
+import { Order } from './order.model';
+import { Observable } from '../../../node_modules/rxjs/Observable';
 
 @Injectable()
 export class OrderService{
 
-    constructor(private cartService: ShoppingCartService){}
+    constructor(private cartService: ShoppingCartService, private http: Http){}
 
     cartItems(): CartItem[]{
         return this.cartService.items
@@ -25,5 +28,9 @@ export class OrderService{
 
     itemsValue(): number{
         return this.cartService.total()  
+    }
+
+    checkOrder(order: Order): Observable<string>{
+        return  this.http.post()
     }
 }
