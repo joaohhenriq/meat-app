70d410a5070076c4a885cdbc97b772387f29e47d
joaohhenriq <joao_camargo10@hotmail.com>
2018-09-27 14:57:53 -0300

    Mapeando resposta do Post para mostrar o Id e não o objeto, e criando página de Pedido Concluído após a finalização do pedido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,10 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { AboutComponent } from './about/about.component';
 import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
     {path: 'restaurants', component: RestaurantsComponent},
     {path: 'restaurants/:id', component: RestaurantDetailComponent,
         children:[
@@ -18,5 +18,7 @@ export const ROUTES: Routes = [
             {path: 'reviews', component: ReviewsComponent}
         ]    
     },
-    {path: 'order', component: OrderComponent}
+    {path: 'order', component: OrderComponent},
+    {path: 'order-summary', component: OrderSummaryComponent},
+    {path: 'about', component: AboutComponent}
 ]
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,6 +4,7 @@ import { OrderItemsComponent } from './order-items/order-items.component';
 import { RadioOption } from './../shared/radio/radio-option.model';
 import { Component, OnInit } from '@angular/core';
 import { Order, OrderItem } from './order.model';
+import { Router } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'mt-order',
@@ -20,7 +21,7 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão Refeição', value: 'REF'}
   ]
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -49,6 +50,7 @@ export class OrderComponent implements OnInit {
     order.orderItems = this.cartItems()
       .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order).subscribe((orderId: string) => {
+      this.router.navigate(['/order-summary'])
       console.log(`Compra concluída: ${orderId}`)
       this.orderService.clear()
     })
