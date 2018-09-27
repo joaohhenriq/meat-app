a660bcfbd573dad1d497d0f96e91cf12744f54fd
joaohhenriq <joao_camargo10@hotmail.com>
2018-09-27 17:30:26 -0300

    Adicionando procedimento de preloading para todos os módulos, que carregará os modulos secundários do momento em background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { ROUTES } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -37,8 +37,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES),
-    FormsModule, 
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules}),
     SharedModule.forRoot()
   ],
   providers: [{provide: LOCALE_ID, useValue: 'pr-BR'}],
