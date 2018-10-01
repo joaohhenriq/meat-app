1a4d08041f9e08e452e9938a342093562d2fbb53
joaohhenriq <joao_camargo10@hotmail.com>
2018-09-30 22:33:14 -0300

    Adicionando animação de entrada e saída para o snackbar

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-snackbar',
   templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.css']
+  styleUrls: ['./snackbar.component.css'],
+  animations: [
+    trigger('snack-visibility', [
+      state('hidden', style({
+        opacity: 0,
+        bottom: '0px',
+      })),
+      state('visible', style({
+        opacity: 1,
+        bottom: '30px'
+      })),
+      transition('hidden => visible', animate('500ms 0s ease-in')),
+      transition('visible => hidden', animate('500ms 0s ease-out'))
+    ])
+  ]
 })
 export class SnackbarComponent implements OnInit {
+
+  snackVisibility: string = 'hidden'
 
   message: string = "Hello there!"
 
@@ -13,5 +30,4 @@ export class SnackbarComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
