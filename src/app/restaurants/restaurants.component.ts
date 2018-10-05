b104eb7985d6598d51bd022d5e5d04174b650dfd
joaohhenriq <joao_camargo10@hotmail.com>
2018-10-05 09:17:37 -0300

    Tratando erro caso o backend caia no meio da pesquisa, e adicionando mensagem de item não encontrado

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -7,6 +7,9 @@ import 'rxjs/add/operator/debounceTime'
 import 'rxjs/add/operator/do'
 import 'rxjs/add/operator/switchMap'
 import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
+import {Observable} from 'rxjs/Observable'
 
 @Component({
   selector: 'mt-restaurants',
@@ -48,7 +51,9 @@ export class RestaurantsComponent implements OnInit {
     this.searchControl.valueChanges
       .debounceTime(500)
       .distinctUntilChanged()
-      .switchMap(searchTerm => this.restaurantService.resturants(searchTerm))
+      .switchMap(searchTerm => this.restaurantService
+        .resturants(searchTerm)
+        .catch(error=>Observable.from([])))
       .subscribe(restaurants => this.restaurants = restaurants)
 
     this.restaurantService.resturants()
