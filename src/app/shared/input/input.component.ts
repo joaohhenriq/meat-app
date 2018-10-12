9208f47a8c625715c882ee58fa4656b9308460a3
joaohhenriq <joao_camargo10@hotmail.com>
2018-10-12 10:40:01 -0300

    Criando o componente de login, utilizando do input e Reactive Forms

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -11,6 +11,7 @@ export class InputComponent implements OnInit {
   input: any
   @Input() label: string
   @Input() errorMessage: string
+  @Input() showTip: boolean = true
 
   @ContentChild(NgModel) model: NgModel
   @ContentChild(FormControlName) control: FormControlName
